624ba439f3538b95e25321d211d0904daecbf6b9
Atul <17singhatul@gmail.com>
2025-08-14 16:30:43 +0530

    updated

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get('/', (req, res)=>{
 
 app.post('/submit', (req, res) => {
   const data = req.body;
+  let existingData = [];
+    if (fs.existsSync('data.json')) {
+    existingData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+  }
+
+  existingData.push(data);
   fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
   res.send('<h1>Message Saved </h1><a href="/">Go Back</a>');
 });
