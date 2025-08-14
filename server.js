a47cd2d3662826d2224d7004b4df17b56884c671
Atul <17singhatul@gmail.com>
2025-08-14 16:16:13 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const port = 3000;
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname,'index.html'))
+})
+
+app.post('/submit', (req, res) => {
+  const data = req.body;
+  fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
+  res.send('<h1>Message Saved </h1><a href="/">Go Back</a>');
+});
+
+app.listen(port, ()=>{
+    console.log('Server started at 3000!!')
+})
