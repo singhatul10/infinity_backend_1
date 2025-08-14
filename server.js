9beb250d33529153da1759ab51144b1f416701d1
Atul <17singhatul@gmail.com>
2025-08-14 17:04:02 +0530

    updated

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(__dirname));
 
 app.get('/', (req, res)=>{
